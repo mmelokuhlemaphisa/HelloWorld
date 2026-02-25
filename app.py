b182b815d0d78f326c707d78d2115ce3195887f9
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-02-25 09:31:37 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+
+
+print("*" * 10)
+
+# Types of variables
+# Strings
+
+course = "Python Programming"
+print (len(course))
+print(course[0])
+print(course[-3])
+print(course[0:6])
+
+print (course.upper())
+print (course.lower())
+print (course.title())
+print (course.strip())
+print (course.find("P"))
+print (course.replace("P", "J"))
+print ("python" in course)
+
